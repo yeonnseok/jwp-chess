2daba8f2a8ea50d13d26cc46da04b5d977e53e03
yeonnseok <ljinsk3@gmail.com>
2020-06-11 10:59:41 +0900

    feat: index page 연동

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -1,12 +1,28 @@
 package chess.controller;
 
+import chess.service.BoardService;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChessController {
-    @GetMapping("/")
+
+    private final BoardService boardService;
+
+    public ChessController(final BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
     public String index() {
         return "index";
+    }
+
+    @GetMapping(value = "/start", produces = MediaType.TEXT_HTML_VALUE)
+    public String start(Model model) {
+        model.addAttribute("squares", boardService.createBoard());
+        return "board";
     }
 }
